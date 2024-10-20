54592d2d87bc52aae7b988b33b18216c71a7c893
Spirule-dev <matthieu.fergola@gmail.com>
2024-10-20 15:05:29 +0200

    base classes added

--- src/DevwayGenericShopBundle.php
+++ src/DevwayGenericShopBundle.php
@@ -2,9 +2,19 @@
 
 namespace Devway\GenericShop;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Devway\GenericShop\DependencyInjection\DevwayGenericShopExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class DevwayGenericShopBundle extends Bundle
+class DevwayGenericShopBundle extends AbstractBundle
 {
+    public function getPath(): string
+    {
+        return dirname(__DIR__);
+    }
 
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        return new DevwayGenericShopExtension();
+    }
 }
